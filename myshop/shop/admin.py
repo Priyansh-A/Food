923d7a773d378dfe422840e7c352fc36d22ee722
Priyanshu Awasthi <nppriyansh112@gmail.com>
2025-10-20 16:18:11 +0545

    Online food ordering site

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -3,3 +3,14 @@ from .models import Category, Product
 # Register your models here.
 
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title','slug']
+    prepopulated_fields = {'slug': ('title',)}
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','slug','price','available','created','updated']
+    list_filter = ['available','created','updated']
+    list_editable = ['price','available']
+    prepopulated_fields = {'slug':('title',)}
