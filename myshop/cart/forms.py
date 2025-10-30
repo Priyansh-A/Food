f670f5ec6775ddaa86351475dcf08d46d8ac2f1d
Priyanshu Awasthi <nppriyansh112@gmail.com>
2025-10-30 10:29:16 +0545

    cart features

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -3,14 +3,17 @@ from django import forms
 PRODUCT_QUANTITY_CHOICES = [(i , str(i)) for i in range(1,21)]
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(
-        choices = PRODUCT_QUANTITY_CHOICES,
-        coerce=int
+    quantity = forms.IntegerField(
+        widget=forms.NumberInput(attrs={
+            'class': 'quantity-input compact-orange',
+            'min': '1',
+            'style': 'width: 35px; color:orange; margin:5px;'
+        })
     )
     override = forms.BooleanField(
         required=False,
         initial=False,
-        widget=forms.HiddenInput
+        widget=forms.HiddenInput(attrs={'class': 'override-field'})
     )
     
     
