502146f1cbe780fa118464f0067b68ecee40f15c
Priyanshu Awasthi <nppriyansh112@gmail.com>
2025-11-12 12:57:25 +0545

    order success details page

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import redirect, render
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import redirect, render, get_object_or_404
 from cart.cart import Cart
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
 
 def order_create(request):
     cart = Cart(request)
@@ -29,3 +30,11 @@ def order_create(request):
             'form': form
             },
         )
+    
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(
+        request, 'admin/orders/order/detail.html', {'order':order}
+    )
+    
